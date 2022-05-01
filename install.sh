fdedaeddb04b0db0c7ba3e905236adbb0f5bf1af
JK <95693701+Jk01012001@users.noreply.github.com>
2022-05-01 15:53:08 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
+#install basic xfce
 sudo dnf install xorg-x11-server-Xorg xorg-x11-xinit network-manager-applet\
  xorg-x11-drv-libinput mesa-dri-drivers xfce4-panel xfce4-datetime-plugin\
- xfce4-session xfce4-settings kitty xfconf xfdesktop\
+ xfce4-session xfce4-settings xfconf xfdesktop\
  xfce4-appfinder xfce4-power-manager xfce4-pulseaudio-plugin pulseaudio\
- gvfs lightdm-gtk xfwm4 NetworkManager-wifi polibar rofi neofetch micro firefox
+ gvfs lightdm-gtk xfwm4 NetworkManager-wifi
+ 
+#install basic stuff
+sudo dnf install kitty polibar rofi neofetch micro firefox
 
-systemctl enable lightdm; # Enable login using graphical interface
-systemctl set-default graphical.target; # Boot to graphical interface as default
+sudo systemctl enable lightdm; # Enable login using graphical interface
+sudo systemctl set-default graphical.target; # Boot to graphical interface as default
+
+
